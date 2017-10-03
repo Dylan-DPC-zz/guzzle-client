500589d00aa1db33a74c990391b3f7c5f7126f3f
Henrique Duarte <bick3@terra.com.br>
2017-10-03 20:14:38 -0300

    remove setup

--- tests/Functional/initializeTest.php
+++ tests/Functional/initializeTest.php
@@ -4,10 +4,6 @@ namespace PHPUnit\Framework;
 
 class initializeTest extends TestCase
 {
-    protected function setUp()
-    {
-        parent::setUp();
-    }
 
     /** @test **/
 	function phpunit_works()
