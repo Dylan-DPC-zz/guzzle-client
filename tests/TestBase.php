a1e718ee381d546abe350a6186a01d7d4443973a
Henrique Duarte Coelho <bick3@terra.com.br>
2017-10-05 04:15:29 -0300

    Add PHPUnit (#19)
    * Add PHPUnit
    
    * remove setup
    
    * Fix Tests
    
    * Guzzle test /w Orchestra
    
    * Fix Tests
    
    * Delete Init Test
    
    * Remove orchestra/database dependency

---
+++ tests/TestBase.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Dpc\GuzzleClient\Tests;
+
+use Orchestra\Testbench\TestCase;
+
+class TestBase extends TestCase
+{
+    protected function setUp()
+    {
+        parent::setUp();
+    }
+}
